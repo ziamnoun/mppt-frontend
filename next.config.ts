61f6fd4414cede851956db243ba229677bd5f58d
ziamnoun <ziamziam05@gmail.com>
2025-12-31 17:33:22 +0600

    Slightly changed

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbpacK: {
-    root: __dirname,
-  },
-}
+  // turboPack: {
+  //   root: __dirname,  
+  // },
+};
 
-module.exports = nextConfig
+export default nextConfig;
